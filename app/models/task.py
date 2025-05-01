fa1eac9cf37eaabed749ee22db61c0a0a5514000
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-01 15:10:46 -0700

    Fix Task to_dict to match required response format (no completed_at)

--- app/models/task.py
+++ app/models/task.py
@@ -10,18 +10,18 @@ class Task(db.Model):
     description: Mapped[str]
     completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     
-    def to_dict(self):
-        return {
-        "id": self.id,
-        "title": self.title,
-        "description": self.description,
-        "completed_at": self.completed_at,
-        "is_complete": self.completed_at is not None # # If completed_at has a value → is_complete = True , If it's None → is_complete = False
-    }
     @classmethod
     def from_dict(cls, task_data):
-        new_task = cls(title=task_data["title"],
-                       description=task_data["description"],
-                       completed_at=task_data.get("completed_at"))
-        return new_task
-        
+        return cls(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data.get("completed_at")  # will be None if null
+        )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.completed_at is not None
+        }
