31a6e68e39e3c3eb121838df7af474396f6024d4
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-04 18:11:07 -0700

    Add GET /books route to retrieve all books with optional query filtering

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.task import Task
+from app import db
 from .route_utilities import create_model_from_dict
+from .route_utilities import validate_model
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks") # main rout
 
@@ -9,3 +11,28 @@ tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks") # main rout
 def create_task():
     request_body = request.get_json()  
     return create_model_from_dict(Task, request_body)
+
+# get all books
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task)
+    
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Task.title.ilike(f"%{title_param}%"))
+        
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Task.description.ilike(f"%{description_param}%"))
+    
+    is_complete_param = request.args.get("is_complete")
+    if is_complete_param:
+        query = query.where(Task.completed_at.isnot(None if is_complete_param == True else True))
+    
+    tasks = db.session.scalars(query.order_by(Task.id))
+    
+    
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return tasks_response
