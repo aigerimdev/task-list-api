1aae7b397d37954f4d980e4603927e8d3a8d7b52
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-04 18:24:02 -0700

    feat: fix route name conflict and add update/delete task functionality

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -39,7 +39,29 @@ def get_all_tasks():
 
 # get one task
 @tasks_bp.get("/<task_id>")
-def get_one_book(task_id):
+def get_one_task(task_id):
     task = validate_model(Task, task_id)
     
     return task.to_dict()
+
+# update task
+@tasks_bp.put("/<task_id>")
+def update_task(task_id):
+    task =validate_model(Task, task_id)
+    request_body = request.get_json()
+    
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    
+    db.session.commit()
+    
+    return Response(status=204, mimetype="application/json")
+
+# delete task
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_model(Task, task_id)
+    db.session.delete(task)
+    db.session.commit()
+    
+    return Response(status=204, mimetype="application/json")
