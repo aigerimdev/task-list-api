a617c1d3c4e27ff4bec43e83bb9e62433db76e19
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-08 15:18:23 -0700

    add id for to_dict method

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
-
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
@@ -13,5 +12,6 @@ class Goal(db.Model):
     
     def to_dict(self):
         return {
+            "id": self.id,
             "title": self.title
         }
