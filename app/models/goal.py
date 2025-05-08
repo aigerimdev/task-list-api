3ce09d133888ab045f01bb24ecf599bfedb2c1a0
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-08 01:10:33 -0700

    creates a Goal model and classmethod: from_dict, instance method: to_dict

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,15 @@ from ..db import db
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    
+    @classmethod
+    def from_dict(cls, goal_data):
+        return cls(
+            title=goal_data["title"]
+        )
+    
+    def to_dict(self):
+        return {
+            "title": self.title
+        }
