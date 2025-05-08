25c6d128397647ae8ac0f3ebe1300cb1bfbc4b39
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-08 15:36:44 -0700

    feat: add one-to-many relationship between goals and tasks

--- app/models/task.py
+++ app/models/task.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from ..db import db
 from typing import Optional
 from sqlalchemy import DateTime
@@ -9,8 +10,8 @@ class Task(db.Model): # specificheskii model dlya sql
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
-    # completed_at: Mapped[datetime] = mapped_column(nullable=True)
-    # completed_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
     
     @classmethod
     def from_dict(cls, task_data): # task_data = dictionary data
