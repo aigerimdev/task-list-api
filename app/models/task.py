3e9441c8f9eac6aa0926783c03f5e03290f53127
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-08 20:09:26 -0700

    remove draft parts

--- app/models/task.py
+++ app/models/task.py
@@ -5,7 +5,7 @@ from typing import Optional
 from sqlalchemy import DateTime
 from datetime import datetime
 
-class Task(db.Model): # specificheskii model dlya sql
+class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
@@ -14,21 +14,13 @@ class Task(db.Model): # specificheskii model dlya sql
     goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
     
     @classmethod
-    def from_dict(cls, task_data): # task_data = dictionary data
+    def from_dict(cls, task_data):
         return cls(
             title=task_data["title"],
             description=task_data["description"],
-            completed_at=task_data.get("completed_at")  # will be None if null
+            completed_at=task_data.get("completed_at") # True if task is done
         )
 
-    # def to_dict(self):
-    #     return {
-    #         "id": self.id,
-    #         "title": self.title,
-    #         "description": self.description,
-    #         "is_complete": self.completed_at is not None
-    #         # "completed_at": self.completed_at.isoformat() if self.completed_at else None
-    #     }
     def to_dict(self, include_goal_id=False):
         task_dict = {
             "id": self.id,
@@ -36,6 +28,8 @@ class Task(db.Model): # specificheskii model dlya sql
             "description": self.description,
             "is_complete": self.completed_at is not None
         }
+        
+        # Only include goal_id if requested
         if include_goal_id:
             task_dict["goal_id"] = self.goal_id
         return task_dict
