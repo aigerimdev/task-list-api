e4214ed30e558cfd3050fc11db05f33ff3572bc6
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-05-08 20:08:33 -0700

    remove draf parts

--- app/models/goal.py
+++ app/models/goal.py
@@ -11,10 +11,12 @@ class Goal(db.Model):
             title=goal_data["title"]
         )
     
-    def to_dict(self):
-        return {
+    def to_dict(self, include_tasks=False):
+        goal_dict = {
             "id": self.id,
-            "title": self.title,
-            # "goal_id": self.goal_id
-            # "tasks": [task.to_dict() for task in self.tasks]
+            "title": self.title
         }
+        # # Add list of tasks only if requested
+        if include_tasks:
+            goal_dict["tasks"] = [task.to_dict(include_goal_id=True) for task in self.tasks]
+        return goal_dict
